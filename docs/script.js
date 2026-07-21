8ce40dd48ae4add0787b59236fc6bc5dd331fe47
Dillersalda <dillerestudos@gmail.com>
2026-07-21 19:59:02 +0000

    Atualizações

--- docs/script.js
+++ docs/script.js
@@ -2,5 +2,9 @@ window.addEventListener("scroll", () =>
 {
 
     const header = document.querySelector(.header);
-    if(win)
-})
+    if(window.scrollY > 150){
+        header.classList.add("ativo");
+    }else{
+        header.classList.remove("ativo");
+    }
+});
